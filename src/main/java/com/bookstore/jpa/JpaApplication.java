d9610e6a2d4de0e3ff96cc45343886b2a24745bd
Isaacmacielbe <inaqtage@gmail.com>
2025-06-19 11:00:44 -0300

    error publisher

--- src/main/java/com/bookstore/jpa/JpaApplication.java
+++ src/main/java/com/bookstore/jpa/JpaApplication.java
@@ -2,8 +2,12 @@ package com.bookstore.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.bookstore.jpa.repositories")
+@EntityScan(basePackages = "com.bookstore.jpa.models")  // ESSENCIAL!)
 public class JpaApplication {
 
 	public static void main(String[] args) {
